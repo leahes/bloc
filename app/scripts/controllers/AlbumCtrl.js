10073835871a9f29e601897eb38839c151d3c832
Leah <leahstuckey@me.com>
2017-10-06 18:45:38 -0500

    Post-Submit wip5

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,1 +1,9 @@
+(function() {
+      function AlbumCtrl(){
+        this.albumData = albumPicasso;
+      }
 
+      angular
+          .module('blocJams')
+          .controller('AlbumCtrl', AlbumCtrl);
+    })();
