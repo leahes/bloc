7904880a3081e2344dd71337e02220f3f65e41d7
Leah <leahstuckey@me.com>
2017-10-20 14:58:02 -0500

    angular-8-work

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,14 +1,11 @@
 (function() {
-  function SongPlayer() {
+  function SongPlayer(Fixtures) {
     var SongPlayer = {};
 
-    var currentSong = null;
-    /**
-    * @desc Buzz object audio file
-    * @type {Object}
-    */
-    var currentBuzzObject = null;
+    var currentAlbum = Fixtures.getAlbum();
 
+    var currentBuzzObject = null;
+    
     /**
     * @function setSong
     * @desc Stops currently playing song and loads new audio file as currentBuzzObject
@@ -18,7 +15,7 @@
     var setSong = function(song) {
       if (currentBuzzObject) {
         currentBuzzObject.stop();
-        currentSong.playing = null;
+        songPlayer.currentSong.playing = null;
       }
 
       currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -26,7 +23,7 @@
         preload: true
       });
 
-      currentSong = song;
+      SongPlayer.currentSong = song;
     };
 
     var playSong = function(song){
@@ -34,35 +31,67 @@
       song.playing = true;
     }
 
-    // @function SongPlayer.play
-    // @desc
+    var getSongIndex = function(song) {
+      return currentAlbum.songs.indexof(song);
+    };
+
+    SongPlayer.currentSong = null;
+
+    /**
+    * @desc Buzz object audio file
+    * @type {Object}
+    */
+
+    // @function play
+    // @desc Play current or new song
     // @param {Object} song
 
     SongPlayer.play = function(song) {
-      if (currentSong !== song) {
+      song = song || SongPlayer.currentSong;
+      if (SongPlayer.currentSong !== song) {
         setSong(song);
         playSong(song);
-      } else if (currentSong === song) {
+      } else if (SongPlayer.currentSong === song) {
         if (currentBuzzObject.isPaused()) {
-            playSong(song);
+          playSong(song);
         }
       }
     };
 
-    // @function SongPlayer.pause
-    // @desc
+    // @function pause
+    // @desc Pause current song
     // @param {Object} song
 
 
     SongPlayer.pause = function(song) {
+      song = song || SongPlayer.currentSong;
       currentBuzzObject.pause();
       song.playing = false;
     };
+
+    SongPlayer.previous = function() {
+      var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+      currentSongIndex--;
+
+      if (currentSongIndex < 0) {
+        currentBuzzObject.stop();
+        SongPlayer.currentSong.playing = null;
+      } else {
+        var song = currentAlbum.songs[currentSongIndex];
+        setSong(song);
+        playSong(song);
+      }
+    };
+    // get the index of the currently playing song and then decrease
+    // that index by one.
 
     return SongPlayer;
   }
 
   angular
   .module('blocJams')
-  .factory('SongPlayer', SongPlayer);
+  .factory('SongPlayer',['Fixtures', SongPlayer]);
 })();
+
+//ALEX? [Violation] Added non-passive event listener to a scroll-blocking 'mousewheel' event. Consider marking event handler as
+// 'passive' to make the page more responsive.
