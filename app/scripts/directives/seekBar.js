2929dc58b519ef24a8af41743d9a820d16d54f26
Leah <leahstuckey@me.com>
2017-10-24 14:15:12 -0500

    wip A-10-W

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -14,18 +14,34 @@
       templateUrl: '/templates/directives/seek_bar.html',
       replace: true,
       restrict: 'E',
-      scope: { },
+      scope: {
+        onChange: '&'
+      },
       link: function(scope, element, attributes){
         scope.value = 0;
         scope.max = 100;
 
         var seekBar = $(element);
 
+        attributes.$observe('value', function(newValue) {
+          scope.value = newValue;
+        });
+
+        attributes.$observe('max', function(newValue) {
+          scope.max = newValue;
+        });
+
         var percentString = function () {
           var value = scope.value;
           var max = scope.max;
           var percent = value / max * 100;
           return percent + "%";
+        };
+
+        var notifyOnChange = function(newValue) {
+          if (typeof scope.onChange === 'function') {
+            scope.onChange({value: newValue});
+          }
         };
 
         scope.fillStyle = function() {
@@ -39,6 +55,7 @@
         scope.onClickSeekBar = function(event) {
           var percent = calculatePercent(seekBar, event);
           scope.value = percent * scope.max;
+          notifyOnChange(scope.value);
         };
 
         scope.trackThumb = function() {
@@ -46,6 +63,7 @@
             var percent = calculatePercent(seekBar, event);
             scope.$apply(function() {
               scope.value = percent * scope.max;
+              notifyOnChange(scope.value);
             });
           });
 
